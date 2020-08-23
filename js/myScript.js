6e899c1de25d6d8987959fc0f38e7d8e44ac5073
xdany01 <42984156+xdany01@users.noreply.github.com>
2020-08-23 04:28:53 -0300

    Initial commit

---
+++ js/myScript.js
@@ -0,0 +1,22 @@
+var params = {
+    container: document.getElementById('lottie'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    animationData: data,
+};
+
+var anim;
+var cerrar = false;
+var e = document.getElementById('lottie');
+anim = lottie.loadAnimation(params);
+e.addEventListener('click', () => {
+    if (cerrar == false) {
+        anim.setSpeed(3);
+        anim.playSegments([0,59], true);
+        cerrar = true;
+    } else {
+        anim.playSegments([30,0], true);
+        cerrar = false
+    }
+});
